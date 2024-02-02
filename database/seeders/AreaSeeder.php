cb8064d50034281a25e8c00f8f53efc1de5ffad6
destafajri <>
2024-02-02 11:31:50 +0700

    Fix Feature Test

--- database/seeders/AreaSeeder.php
+++ database/seeders/AreaSeeder.php
@@ -14,26 +14,32 @@ class AreaSeeder extends Seeder
     public function run(): void
     {
         $area1 = new Area();
+        $area1->id = 1;
         $area1->area_name = 'Jakarta';
         $area1->save();
 
         $area2 = new Area();
+        $area2->id = 2;
         $area2->area_name = 'Bandung';
         $area2->save();
 
         $area3 = new Area();
+        $area3->id = 3;
         $area3->area_name = 'Jogja';
         $area3->save();
 
         $area4 = new Area();
+        $area4->id = 4;
         $area4->area_name = 'Semarang';
         $area4->save();
 
         $area5 = new Area();
+        $area5->id = 5;
         $area5->area_name = 'Surabaya';
         $area5->save();
 
         $area6 = new Area();
+        $area6->id = 6;
         $area6->area_name = 'Bali';
         $area6->save();
     }
